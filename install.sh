60d43ff68d3cd7bb85e196bff5b39208e9fcc51f
DanSnow <dododavid006@gmail.com>
2015-11-03 11:27:16 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,42 +1,8 @@
 #!/bin/bash
 
-prompt_yn() {
-  echo "$1"
-  select yn in "Yes" "No"; do
-    case $yn in
-      Yes)
-        return 0
-        ;;
-      No)
-        return 1
-        ;;
-    esac
-  done
-  return false
-}
-
-ask_path() {
-  read -p "$1" -e -i '~' path
-  if [[ -z path ]]; then
-    path="$HOME"
-  fi
-}
-
-git 1> /dev/null 2>&1
-if [ $? -eq 127 ]; then
-  echo 'git not found. abort'
-  exit 1
-fi
-
 path="$HOME"
 
-# ask_path 'Please input install directory: '
-#
-# if prompt_yn "Do you want to remove old files?"; then
-#   rm -rf "$path/.vimrc" "$path/.vim"  "$path/.gvimrc"
-# fi
-
-cp -rf .vim .vimrc .gvimrc "$path"
+cp -r .vim .vimrc .gvimrc "$path"
 echo "Now install plugin"
-vim +PlugInstall +qall
+vim +PlugClean! +PlugInstall +qall
 echo 'done'
