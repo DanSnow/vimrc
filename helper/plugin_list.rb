6ee5b46bb24ed044f3f6336a9af5b5456602830a
DanSnow <dododavid006@gmail.com>
2016-11-30 21:01:53 +0800

    Update plugin_list

--- helper/plugin_list.rb
+++ helper/plugin_list.rb
@@ -16,24 +16,24 @@ markdown_lines = []
 
 plugin_list.each do |line|
   if (m = line.match(/^""\s(.*)/))
-    markdown_lines << ''
-    markdown_lines << "## #{m[1]} ##"
+    markdown_lines << "\n"
+    markdown_lines << "## #{m[1]} ##\n"
   elsif (m = line.match(/^"""\s(.*)/))
-    markdown_lines << ''
-    markdown_lines << "### #{m[1]} ###"
+    markdown_lines << "\n"
+    markdown_lines << "### #{m[1]} ###\n"
   elsif (m = line.match(/^\s*Plug\s+'(.*?)'(?:$|,.*)/))
     idx = line.index '/'
     if idx
       url = "https://github.com/#{m[1]}"
-      markdown_lines << "- [#{m[1].sub(%r{^.*/}, '')}](#{url})"
+      markdown_lines << "- [#{m[1].sub(%r{^.*/}, '')}](#{url})\n"
     else
       url = "https://github.com/vim-scripts/#{m[1]}"
-      markdown_lines << "- [#{m[1]}](#{url})"
+      markdown_lines << "- [#{m[1]}](#{url})\n"
     end
   end
 end
 
-readme = File.read(readme_path).each_line.map(&:chomp)
+readme = File.read(readme_path).lines
 idx = readme.index { |line| line =~ /\[comment\]: # Inject start/ }
 if idx.nil?
   STDERR.puts 'Could not found inject point'
@@ -41,5 +41,5 @@ if idx.nil?
   puts markdown_lines
 else
   new_readme = readme.take(idx + 1) + markdown_lines
-  File.write(readme_path, new_readme.join("\n"))
+  File.write(readme_path, new_readme.join)
 end
